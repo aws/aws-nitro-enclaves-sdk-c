855c140ba8a8912c0cf14f91c820c182330667ed
Alexandru Ciobotaru <alcioa@amazon.com>
2020-08-19 00:15:03 +0300

    Add SDK seed_entropy API
    Signed-off-by: Alexandru Ciobotaru <alcioa@amazon.com>
    Reviewed-by: Andrei Cipu <acipu@amazon.com>
    Reviewed-by: Andra-Irina Paraschiv <andraprs@amazon.com>
    Reviewed-by: Bogdan Vasile <bve@amazon.com>

--- include/aws/nitro_enclaves/nitro_enclaves.h
+++ include/aws/nitro_enclaves/nitro_enclaves.h
@@ -5,13 +5,27 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
+#include <aws/nitro_enclaves/exports.h>
+
 #include <aws/common/allocator.h>
 #include <aws/common/macros.h>
 
+/**
+ * TODO: This should be the only external API. Simple and powerful
+ * TODO: Naming convention (i.e. aws_ne_* etc.)
+ */
+
 AWS_EXTERN_C_BEGIN
 
+AWS_NITRO_ENCLAVES_API
 void aws_nitro_enclaves_library_init(struct aws_allocator *allocator);
+
+AWS_NITRO_ENCLAVES_API
 void aws_nitro_enclaves_library_clean_up(void);
+
+AWS_NITRO_ENCLAVES_API
+int aws_nitro_enclaves_library_seed_entropy(uint64_t bytes);
 
 AWS_EXTERN_C_END
 
--- source/nitro_enclaves.c
+++ source/nitro_enclaves.c
@@ -8,6 +8,17 @@
 #include <aws/common/allocator.h>
 #include <aws/http/http.h>
 
+#include <fcntl.h>
+#include <linux/random.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <nsm-lib.h>
+
+/* Maximum number of bytes NSM random response returns. */
+#define NSM_RANDOM_REQ_SIZE (256)
+
 static bool s_library_initialized = false;
 
 void aws_nitro_enclaves_library_init(struct aws_allocator *allocator) {
@@ -29,4 +40,56 @@ void aws_nitro_enclaves_library_clean_up(void) {
 
     aws_auth_library_clean_up();
     aws_http_library_clean_up();
+}
+
+int aws_nitro_enclaves_library_seed_entropy(uint64_t num_bytes) {
+
+    int nsm_fd = nsm_lib_init();
+    if (nsm_fd < 0)
+        return -1;
+
+    int dev_fd = open("/dev/random", O_WRONLY);
+    if (dev_fd < 0) {
+        nsm_lib_exit(nsm_fd);
+        return -1;
+    }
+
+    uint64_t count = 0;
+
+    while (count != num_bytes) {
+        uint8_t buf[NSM_RANDOM_REQ_SIZE];
+        size_t buf_len = sizeof(buf) > (num_bytes - count) ?
+            (num_bytes - count) : sizeof(buf);
+
+        /* Yields up to 256 bytes */
+        int rc = nsm_get_random(nsm_fd, buf, &buf_len);
+        if (rc)
+            goto err;
+
+        if (buf_len == 0) {
+            /* NSM starts yielding zero entropy */
+            goto err;
+        }
+
+        if ((ssize_t)buf_len != write(dev_fd, buf, buf_len))
+            goto err;
+
+        int bits = buf_len * 8;
+        rc = ioctl(dev_fd, RNDADDTOENTCNT, &bits);
+        if (rc < 0)
+            goto err;
+
+        count += buf_len;
+    }
+
+    close(dev_fd);
+    nsm_lib_exit(nsm_fd);
+
+    return 0;
+
+err:
+    close(dev_fd);
+    nsm_lib_exit(nsm_fd);
+
+    return -1;
 }
