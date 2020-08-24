cda0e20ae072d181532a12c7fdb61ff8a732c7ae
Petre Eftime <epetre@amazon.com>
2020-08-24 15:09:08 +0300

    set default nitro_enclaves allocator on library init
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- include/aws/nitro_enclaves/nitro_enclaves.h
+++ include/aws/nitro_enclaves/nitro_enclaves.h
@@ -18,6 +18,12 @@
 
 AWS_EXTERN_C_BEGIN
 
+/**
+ * Initializes the library.
+ *
+ * @param[in]    allocator    Optional parameter to override default allocator. If this parameter is set to null, a
+ *                            default allocator is used instead.
+ */
 AWS_NITRO_ENCLAVES_API
 void aws_nitro_enclaves_library_init(struct aws_allocator *allocator);
 
@@ -26,6 +32,14 @@ void aws_nitro_enclaves_library_clean_up(void);
 
 AWS_NITRO_ENCLAVES_API
 int aws_nitro_enclaves_library_seed_entropy(uint64_t bytes);
+
+/**
+ * Returns the allocator of the library, as set by aws_nitro_enclaves_library_init.
+ *
+ * @return Returns the allocator used by the library.
+ */
+AWS_NITRO_ENCLAVES_API
+struct aws_allocator *aws_nitro_enclaves_get_allocator(void);
 
 AWS_EXTERN_C_END
 
