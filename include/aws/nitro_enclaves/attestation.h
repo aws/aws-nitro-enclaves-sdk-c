2dfa80f381d28de569e8c6a2fe65443b3376afe2
Petre Eftime <epetre@amazon.com>
2020-08-27 13:20:50 +0300

    Fix up attestation helpers to work with AWS-LC
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- include/aws/nitro_enclaves/attestation.h
+++ include/aws/nitro_enclaves/attestation.h
@@ -8,17 +8,21 @@
 #include <aws/nitro_enclaves/exports.h>
 #include <aws/nitro_enclaves/kms.h>
 
+#include <aws/common/allocator.h>
+#include <aws/common/byte_buf.h>
+
 AWS_EXTERN_C_BEGIN
 
 enum aws_rsa_key_size {
-    AWS_RSA_2048,
-    AWS_RSA_3084,
-    AWS_RSA_4096,
+    AWS_RSA_2048 = 2048,
+    AWS_RSA_3072 = 3072,
+    AWS_RSA_4096 = 4096,
 };
 
 struct aws_rsa_keypair {
-    struct aws_byte_buf *public_key;
-    struct aws_byte_buf *private_key;
+    struct aws_allocator *allocator;
+    struct aws_byte_buf public_key;
+    struct aws_byte_buf private_key;
 };
 
 /**
@@ -35,34 +39,36 @@ struct aws_rsa_keypair * aws_attestation_rsa_keypair_new(struct aws_allocator *a
 /**
  * Cleanups internal structures for a previously generated RSA keypair.
  *
- * @param[in]   allocator           The allocator to use.
  * @param[in]   aws_rsa_keypair     The RSA keypair previously allocated via @aws_attestation_keypair_new.
  */
 AWS_NITRO_ENCLAVES_API
-void aws_attestation_rsa_keypair_destroy(struct aws_allocator *allocator, struct aws_rsa_keypair *keypair);
+void aws_attestation_rsa_keypair_destroy(struct aws_rsa_keypair *keypair);
 
 /**
  * Generates attestation data.
  *
- * @param[in]   allocator   The allocator to use.
- * @param[in]   public_key  The public key used for attestation.
+ * @param[in]   allocator        The allocator to use.
+ * @param[in]   public_key       The public key used for attestation.
+ * @param[out]  attestation_doc  The public key used for attestation.
  *
- * @return                  The attestation document as a newly allocated byte buffer.
+ * @return                       Returns the error code. If SUCCESS, then attestation_doc is populated.
  */
 AWS_NITRO_ENCLAVES_API
-struct aws_byte_buf * aws_attestation_request(struct aws_allocator *allocator, struct aws_byte_buf *public_key);
+int aws_attestation_request(struct aws_allocator *allocator, struct aws_rsa_keypair *keypair, struct aws_byte_buf *attestion_doc);
 
 /**
  * Decrypts the provided ciphertext data using the specified private key.
+ * Uses the cipher text allocator.
  *
- * @param[in]   allocator   The allocator to use.
+ * @param[in]   allocator   The allocator used to initialize plaintext.
+ * @param[in]   keypair     The keypair used to decrypt.
  * @param[in]   ciphertext  The ciphertext to decrypt.
- * @param[in]   private_key The private key to decrypt with.
- * 
- * @return                  The decrypted data as newly allocated byte buffer.
+ * @param[out]  plaintext   The decrypted ciphertext.
+ *
+ * @return                  The result of the operation. On SUCCESS, the result will be placed in plaintext.
  */
 AWS_NITRO_ENCLAVES_API
-struct aws_byte_buf *aws_attestation_rsa_decrypt(struct aws_allocator *allocator, struct aws_byte_buf *ciphertext, struct aws_byte_buf *private_key);
+int aws_attestation_rsa_decrypt(struct aws_allocator *allocator, struct aws_rsa_keypair *keypair, struct aws_byte_buf *ciphertext, struct aws_byte_buf *plaintext);
 
 AWS_EXTERN_C_END
 
