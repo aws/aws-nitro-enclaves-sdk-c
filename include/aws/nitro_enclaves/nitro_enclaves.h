5ca7c0401c080f5a3167c7eb39b79cfa8412ff96
Petre Eftime <epetre@amazon.com>
2021-03-12 12:29:32 +0200

    Add additional documentation and build with doxygen
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- include/aws/nitro_enclaves/nitro_enclaves.h
+++ include/aws/nitro_enclaves/nitro_enclaves.h
@@ -1,10 +1,32 @@
 #ifndef AWS_NITRO_ENCLAVES_NITRO_ENCLAVES_H
 #define AWS_NITRO_ENCLAVES_NITRO_ENCLAVES_H
 
-/**
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
+/**
+  * @mainpage
+  *
+  * # AWS Nitro Enclaves SDK for C
+  *
+  * This SDK allows you to use the functionality of AWS Nitro Enclaves and provides simple APIs for seeding
+  * system entropy or calling into AWS KMS using attestation.
+  *
+  * To instantiate the library, call aws_nitro_enclaves_library_init() first.
+  * To seed entropy, use aws_nitro_enclaves_library_seed_entropy().
+  *
+  * ## AWS KMS
+  * To use AWS KMS functionality, create an aws_kms_client using aws_nitro_enclaves_kms_client_new(),
+  * afterwards, call aws_kms_decrypt_blocking(), aws_kms_generate_random_blocking() and
+  * aws_kms_generate_data_key_blocking(), depending on needs.
+  * 
+  * Additional documentation and sample can be found in the main
+  * [Github repository](https://github.com/aws/aws-nitro-enclaves-sdk-c) or
+  * on AWS documentation website for
+  * [AWS Nitro Enclaves](https://docs.aws.amazon.com/enclaves/latest/user/nitro-enclave.html)
+  */
 
 #include <aws/nitro_enclaves/exports.h>
 
@@ -12,9 +34,9 @@
 #include <aws/common/macros.h>
 
 /**
- * TODO: This should be the only external API. Simple and powerful
- * TODO: Naming convention (i.e. aws_ne_* etc.)
- */
+  * @file
+  * Initialize the library and main enclave functionality.
+  */
 
 AWS_EXTERN_C_BEGIN
 
@@ -30,6 +52,14 @@ void aws_nitro_enclaves_library_init(struct aws_allocator *allocator);
 AWS_NITRO_ENCLAVES_API
 void aws_nitro_enclaves_library_clean_up(void);
 
+/**
+ * Seeds the entropy pool of the system with entropy from the NitroSecureModule device attached to the enclave.
+ * In practical situations, the functionality should be called when entropy is required but the system pool
+ * doesn't have sufficient entropy available, or on a given timer, depending on requirements.
+ *
+ * @param[in]    bytes      The number of bytes to seed.
+ * @return                  AWS_OP_SUCCESS if the operation has succeeded.
+ */
 AWS_NITRO_ENCLAVES_API
 int aws_nitro_enclaves_library_seed_entropy(uint64_t bytes);
 
