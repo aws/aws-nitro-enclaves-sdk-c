00baf291367c71ea7db10875d381c90d955f105d
Alex Schoof <aes@squareup.com>
2022-06-29 13:24:54 -0400

    fix lib paths

--- bin/kmstool-enclave-cli/build-musl.sh
+++ bin/kmstool-enclave-cli/build-musl.sh
@@ -5,5 +5,4 @@ set -e
 docker build --target kmstool-enclave-cli -t kmstool-enclave-cli -f ../../containers/Dockerfile.alpine ../..
 CONTAINER_ID=$(docker create kmstool-enclave-cli)
 docker cp $CONTAINER_ID:/kmstool_enclave_cli ./
-docker cp $CONTAINER_ID:/usr/lib64/libnsm.so ./
 docker rm $CONTAINER_ID
