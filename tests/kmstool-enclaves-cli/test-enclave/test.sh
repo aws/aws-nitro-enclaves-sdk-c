d4dcaca60fc6313138a2e70523b365c7c0e5d1da
Richard Fan <richardfan1126@gmail.com>
2023-12-20 16:23:45 +0800

    kmstool-enclave-cli: add test summary in smoke test script
    Signed-off-by: Richard Fan <richardfan1126@gmail.com>

--- tests/kmstool-enclaves-cli/test-enclave/test.sh
+++ tests/kmstool-enclaves-cli/test-enclave/test.sh
@@ -29,6 +29,8 @@ echo "|                                                                         
 echo "|                                                                                    |"
 echo "======================================================================================"
 
+set +e
+
 /app/kmstool_enclave_cli decrypt \
     --region us-east-1 \
     --proxy-port 8000 \
@@ -36,6 +38,14 @@ echo "==========================================================================
     --aws-secret-access-key ${SECRET_ACCESS_KEY} \
     --aws-session-token ${SESSION_TOKEN} \
     --ciphertext ${CIPHERTEXT}
+
+if [[ $? -eq 0 ]];then
+    DECRYPT_SUCCESS=1
+else
+    DECRYPT_SUCCESS=0
+fi
+
+set -e
 
 # Run genkey
 echo "======================================================================================"
@@ -52,6 +62,8 @@ echo "|                                                                         
 echo "|                                                                                    |"
 echo "======================================================================================"
 
+set +e
+
 /app/kmstool_enclave_cli genkey \
     --region us-east-1 \
     --proxy-port 8000 \
@@ -59,8 +71,15 @@ echo "==========================================================================
     --aws-secret-access-key ${SECRET_ACCESS_KEY} \
     --aws-session-token ${SESSION_TOKEN} \
     --key-id ${KMS_KEY_ARN} \
-    --key-spec "AES-256" \
-    --length 1024
+    --key-spec "AES-256"
+
+if [[ $? -eq 0 ]];then
+    GENKEY_SUCCESS=1
+else
+    GENKEY_SUCCESS=0
+fi
+
+set -e
 
 # Run genrandom
 echo "======================================================================================"
@@ -77,6 +96,8 @@ echo "|                                                                         
 echo "|                                                                                    |"
 echo "======================================================================================"
 
+set +e
+
 /app/kmstool_enclave_cli genrandom \
     --region us-east-1 \
     --proxy-port 8000 \
@@ -85,20 +106,52 @@ echo "==========================================================================
     --aws-session-token ${SESSION_TOKEN} \
     --length 1024
 
+if [[ $? -eq 0 ]];then
+    GENRANDOM_SUCCESS=1
+else
+    GENRANDOM_SUCCESS=0
+fi
+
+set -e
+
 echo "======================================================================================"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
-echo "|                                   TEST: SUCCESS                                    |"
+echo "|                                    TEST RESULT                                     |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "|                                                                                    |"
 echo "======================================================================================"
-echo "TEST: SUCCESS"
+
+if [[ ${DECRYPT_SUCCESS} == 1 ]]; then
+    echo "Decrypt: SUCCESS"
+else
+    echo "Decrypt: FAIL"
+fi
+
+if [[ ${GENKEY_SUCCESS} == 1 ]]; then
+    echo "GenKey: SUCCESS"
+else
+    echo "GenKey: FAIL"
+fi
+
+if [[ ${GENRANDOM_SUCCESS} == 1 ]]; then
+    echo "GenRandom: SUCCESS"
+else
+    echo "GenRandom: FAIL"
+fi
+
+echo "================"
+if [[ ${DECRYPT_SUCCESS} == 1 && ${GENKEY_SUCCESS} == 1 && ${GENRANDOM_SUCCESS} == 1 ]]; then
+    echo "Status: SUCCESS"
+else
+    echo "Status: FAIL"
+fi
 
 # Wait before exit so the parent instance can inspect the log via console
 sleep 10
