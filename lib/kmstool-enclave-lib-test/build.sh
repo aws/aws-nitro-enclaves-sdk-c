a0360336ca1f704d7448f86eafb22e78c6856531
ericlurker <96100006+ericlurker@users.noreply.github.com>
2025-03-04 15:28:32 +0800

    fix build error

--- lib/kmstool-enclave-lib-test/build.sh
+++ lib/kmstool-enclave-lib-test/build.sh
@@ -4,6 +4,7 @@ set -e
 
 docker build --target kmstool-enclave-lib-test -t kmstool-enclave-lib-test -f ../../containers/Dockerfile.al2 ../..
 CONTAINER_ID=$(docker create kmstool-enclave-lib-test)
+docker cp $CONTAINER_ID:/libkmstool-enclave-lib.so ./
 docker cp $CONTAINER_ID:/kmstool_enclave_lib_test ./
 docker cp $CONTAINER_ID:/usr/lib64/libnsm.so ./
 docker rm $CONTAINER_ID
