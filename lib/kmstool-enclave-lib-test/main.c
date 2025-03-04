dd2f4661554d3d627539ce515a9034df4e794ac6
ericlurker <96100006+ericlurker@users.noreply.github.com>
2025-03-04 18:03:33 +0800

    fix build error

--- lib/kmstool-enclave-lib-test/main.c
+++ lib/kmstool-enclave-lib-test/main.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
             "9+V8pysXJbZf0HZsffLI91T3AOM5XFMkQp+ynSF8ao92FkLVuX+4A==",
         .key_id = "60b8ce3a-7466-42b7-96a7-a3868f0fd1bf",
         .encryption_algorithm = "SYMMETRIC_DEFAULT",
-        .with_logs = 0,
+        .with_logs = 1,
     };
 
     kmstool_enclave_init(&params_init);
