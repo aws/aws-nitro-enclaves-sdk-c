e0f8c1be67c8bb40b72149fbeade23965d6f86dc
ericlurker <96100006+ericlurker@users.noreply.github.com>
2025-03-04 17:36:30 +0800

    fix build error

--- lib/kmstool-enclave-lib/source/kmstool_decrypt.c
+++ lib/kmstool-enclave-lib/source/kmstool_decrypt.c
@@ -3,11 +3,11 @@
 /* Decrypt the given base64 encoded ciphertext via KMS and output its base64 encoded result */
 static int decrypt_from_kms(
     const struct app_ctx *ctx,
-    const struct kmstool_encrypt_params *params,
+    const struct kmstool_decrypt_params *params,
     struct aws_byte_buf *plaintext) {
     ssize_t rc = AWS_OP_ERR;
 
-    struct aws_byte_buf ciphertext = aws_byte_buf_from_array(params->plaintext, params->plaintext_len);
+    struct aws_byte_buf ciphertext = aws_byte_buf_from_array(params->ciphertext, params->ciphertext_len);
 
     /* Decrypt the data with KMS. */
     rc = aws_kms_decrypt_blocking(ctx->kms_client, ctx->key_id, ctx->encryption_algorithm, &ciphertext, plaintext);
